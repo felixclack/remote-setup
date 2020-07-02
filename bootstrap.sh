1e315b5f9bb63c365e5b1d92d124122c7a8fc5b0
Felix Clack <felixclack@gmail.com>
2020-07-02 13:57:39 +1000

    Use rvm to install Ruby

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,8 @@ curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
+rvm install 2.7.1
+
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 
 exec /bin/bash --login
